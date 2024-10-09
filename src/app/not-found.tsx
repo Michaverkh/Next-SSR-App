e74518c990a4d9e27a6613cc0d6ec12876b91763
Michaverkh <michaverkh@gmail.com>
2024-10-09 08:25:55 +0300

    designing pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import Link from 'next/link';
+
+const Error = () => {
+	return (
+		<>
+			<div>Not Found</div>
+			<Link href='/'>Go to Home</Link>
+		</>
+	);
+};
+
+export default Error;
